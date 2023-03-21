c0cb5de3c8b1170dc45387c1588975827f6b72ab
udonggi <dongki1882@naver.com>
2023-03-21 13:44:14 +0900

    2-15질문등록 폼

--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -3,10 +3,7 @@ package com.mysite.sbb.question;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -29,5 +26,16 @@ public class QuestionController {
         Question question = questionService.getQuestion(id);
         model.addAttribute("question",question);
         return "question_detail";
+    }
+
+    @GetMapping("/create")
+    public String questionCreate() {
+        return "question_form";
+    }
+
+    @PostMapping("/create")
+    public String questionCreate2(@RequestParam String subject, @RequestParam String content) {
+        // TODO 질문을 저장
+        return "redirect:/question/list"; //질문 저장 후 저장목록으로 이동
     }
 }
